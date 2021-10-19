ac3044cfa77df1b3604b8c65b73609c7ea47ca52
Pam <pamela.glownia@gmail.com>
2021-10-19 20:18:47 +0200

    Initial commit

---
+++ src/main/java/pl/glownia/pamela/App.java
@@ -0,0 +1,13 @@
+package pl.glownia.pamela;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
